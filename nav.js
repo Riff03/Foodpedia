f6a416fdb8f1ba21e1957e7808e5b7a44f5cd154
Achmad Rifki <a.rifki180603@gmail.com>
2023-05-22 16:18:56 +0700

    Memanipulasi data

--- nav.js
+++ nav.js
@@ -1,3 +1,15 @@
-document.querySelector('.menu-trigger').addEventListener('click', function() =>{
-    console.log('test')
+let open = false
+
+document.querySelector('.menu-trigger').addEventListener('click', function() {
+    if(!open){
+        document.querySelector('menu').setAttribute('style','transform','translateX(0)')
+        document.querySelector('.search-box').setAttribute('style','display: block')
+    }else{
+        document.querySelector('.menu').setAttribute('style','transform','translateX(-100%)')
+        document.querySelector('.search-box').setAttribute('style','display: none')
+    }
+    open=!open
 })
+
+const variableA = 10
+let variableB = 20
